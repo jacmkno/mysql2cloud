9e514e52310b16baf85454567038040b6613f8aa
jacmkno <jose@activisual.net>
2024-06-23 23:36:45 -0500

    Update mysql2csv.gz.sh

--- mysql2csv.gz.sh
+++ mysql2csv.gz.sh
@@ -15,15 +15,24 @@ if [[ ! "$table_name" =~ \. ]]; then
   exit 1
 fi
 
-# Create a named pipe
-pipe_name=$(mktemp -u)
+# Extract all arguments except the last one (the table name)
+mysql_args=("${@:1:$#-1}")
+
+# Determine the secure_file_priv setting
+secure_file_priv=$(mysql "${mysql_args[@]}" -N -e "SHOW VARIABLES LIKE 'secure_file_priv'" | awk '{ print $2 }')
+
+# Create a named pipe in the appropriate directory
+if [ -n "$secure_file_priv" ] && [ "$secure_file_priv" != "NULL" ]; then
+  pipe_dir="$secure_file_priv"
+else
+  pipe_dir="/tmp"
+fi
+
+pipe_name=$(mktemp -u "$pipe_dir/mysql_pipe.XXXXXX")
 mkfifo "$pipe_name"
 
 # Set up gzip to read from the pipe and write to a file
 gzip -c < "$pipe_name" > table_name.csv.gz &
-
-# Extract all arguments except the last one (the table name)
-mysql_args=("${@:1:$#-1}")
 
 # Run the MySQL command to export the table to the named pipe
 mysql "${mysql_args[@]}" -e "SELECT * FROM $table_name INTO OUTFILE '$pipe_name' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n';"
